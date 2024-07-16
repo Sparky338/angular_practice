d7b9e02b21e25ded159cba080839de7d9e2d752c
Matthew Hutter <matthewjhutter@gmail.com>
2024-07-16 10:58:00 -0700

    Figuring out NgModules

--- src/app/components/decrement.component.ts
+++ src/app/components/decrement.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
 
 @Component({
+    standalone: true,
     selector: 'decrement-number',
     template: '<button>{{reduce}}</button>',
-    styleUrl: '../CSS/decrement.css'
+    styleUrl: '../CSS/decrement.component.css',
 })
 
 export class DecrementComponent {
     reduce = '-'
+    // button does what? here
 }
