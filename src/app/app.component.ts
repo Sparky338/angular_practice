da0f33bc532c2afd68f8cb677f7b42088b71effd
Matthew Hutter <matthewjhutter@gmail.com>
2024-07-16 21:29:57 -0700

    created increment module, edited app component to reflect Angular docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgModuleModule } from './app.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgModuleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+// import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DecrementComponent } from './components/decrement.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    // BrowserModule,
+    DecrementComponent
+  ],
+  providers:[],
+  bootstrap:[]
+})
+export class NgModuleModule { }
