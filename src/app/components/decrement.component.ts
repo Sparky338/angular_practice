c3d59008f5a71cbd7a47f7cd7dbe0eeebb53b6dc
Matthew Hutter <matthewjhutter@gmail.com>
2024-07-17 20:01:14 -0700

    Set button functionality and idea for button logic

--- src/app/components/decrement.component.ts
+++ src/app/components/decrement.component.ts
@@ -3,11 +3,15 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-decrement',
     standalone: true,
-    template: '<button>{{reduce}}</button>',
+    template: '<button (click)="decreaseScore()">{{reduce}}</button>',
     styleUrl: '../CSS/decrement.component.css',
 })
 
 export class DecrementComponent {
     reduce = '-'
     // button does what? here
+    decreaseScore() {
+        // check that score is not zero, if (score !<= 0)
+        // then decrease score logic here;
+      }
 }
--- src/app/components/increment/increment.component.ts
+++ src/app/components/increment/increment.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-increment',
   standalone: true,
-  imports: [],
-  template: '<button>{{increase}}</button>',
+  imports: [AppComponent],
+  template: '<button (click)="increaseScore()">{{increase}}</button>',
   styleUrl: './increment.component.css'
 })
 export class IncrementComponent {
   increase = "+"
   //button does what? here
+  increaseScore() {
+    // increase score logic here;
+  }
+
 }
