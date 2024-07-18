8495723198a3aaf5f8f40336f16101679f3f2127
Matthew Hutter <matthewjhutter@gmail.com>
2024-07-17 22:30:35 -0700

    connected counterReducer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideStore } from '@ngrx/store';
+import { provideStore, StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideStore(),
+    importProvidersFrom(StoreModule.forRoot({ count: counterReducer }))
   ],
 };
