83acdd84cd8985575f1500f1d8043de83b95b6a5
Matthew Hutter <matthewjhutter@gmail.com>
2024-07-17 23:26:50 -0700

    attempts to get store number to display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { AppModule } from './app.module';
 import { IncrementComponent } from './components/increment/increment.component';
 import { IncrementModule } from './components/increment/increment.module';
@@ -26,7 +27,16 @@ import { counterReducer } from './counter.reducer';
 export class AppComponent {
   title = 'Gametime Hero score counter';
   count$!: Observable<number>;
-  score = this.count$;
+  score: Observable<number>;
+
+  constructor(private store: Store<{count: number}>) {
+    this.count$ = store.select('count');
+    this.count$.subscribe({
+      next(value) {
+        this.score = value;
+      }});
+  }
+
   // IncrementComponent = IncrementComponent;
   // IncrementModule = IncrementModule;
   // NgModuleModule = NgModuleModule;
