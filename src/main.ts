19d14017eeccb32d03cf8c1632d16f54a5a66423
Matthew Hutter <matthewjhutter@gmail.com>
2024-07-23 12:06:53 -0700

    Firebase additions, WIP

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import {initializeApp} from 'firebase/app';
+import {getDatabase, get, } from 'firebase/database'
+
+const firebaseConfig = {
+
+};
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
