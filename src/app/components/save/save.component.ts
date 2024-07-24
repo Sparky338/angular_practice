624068a4b46eb07e5b3eed31dbf90d30f85906ec
Matthew Hutter <matthewjhutter@gmail.com>
2024-07-23 17:21:05 -0700

    save component set up

--- src/app/components/save/save.component.ts
+++ src/app/components/save/save.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { reset } from '../../counter.actions';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-save',
   standalone: true,
-  imports: [],
-  templateUrl: './save.component.html',
-  styleUrl: './save.component.css'
+  template: '<button mat-raised-button (click)="save()">Save Count</button>',
+  styleUrl: './save.component.css',
+  imports: [MatButtonModule],
 })
 export class SaveComponent {
+  count$!: Observable<number>;
 
+  constructor(private store: Store<{count: number}>) {
+    this.count$ = store.select('count');
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
 }
