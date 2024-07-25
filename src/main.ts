ae0bc9abf9789eb8e86e24ffbaed2f9a7d5651f0
Matthew Hutter <matthewjhutter@gmail.com>
2024-07-25 11:07:29 -0700

    Updated Angular_practice to angular_practice for firebase

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,12 @@ import {initializeApp} from 'firebase/app';
 import {getDatabase, set, ref } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVrDKzQsMhALumJTkj-UGAqw5V8QjutGQ",
-  authDomain: "angular-test-gth.firebaseapp.com",
-  projectId: "angular-test-gth",
-  storageBucket: "angular-test-gth.appspot.com",
-  messagingSenderId: "878489116671",
-  appId: "1:878489116671:web:b3bba92aa02617a10211ca",
-  measurementId: "G-KLJQEMCPQH"
+  apiKey: "AIzaSyA5e7lJ-Ulkjt1fcssrvXOtiFivGQgefQU",
+  authDomain: "angular-practice-gth.firebaseapp.com",
+  projectId: "angular-practice-gth",
+  storageBucket: "angular-practice-gth.appspot.com",
+  messagingSenderId: "340514324925",
+  appId: "1:340514324925:web:b23fcc47721f0106cc92d4"
 };
 
 const app = initializeApp(firebaseConfig);
